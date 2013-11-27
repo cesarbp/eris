7142ff398c3450ff47682eee49b3d9a34787182f
cesarbp <cesar.bolanosportilla@gmail.com>
2013-11-26 23:24:29 -0600

    Initial

---
+++ README.md
@@ -0,0 +1,13 @@
+# Eris
+
+A Leiningen plugin to run your tests under a modified codebase.
+
+## Usage
+
+Under development
+
+## License
+
+Copyright © 2013 César Bolaños
+
+Distributed under the Eclipse Public License the same as Clojure.
---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject eris "0.1.0-SNAPSHOT"
+  :description "Chaos in code to measure test efficacy"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :eval-in-leiningen true)
