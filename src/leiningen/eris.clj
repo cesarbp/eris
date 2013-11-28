35154ae662d9a7470a52b45e4b4da9e65d18b976
cesarbp <cesar.bolanosportilla@gmail.com>
2013-11-28 17:47:30 -0600

    Shape

--- src/leiningen/eris.clj
+++ src/leiningen/eris.clj
@@ -16,28 +16,11 @@
         (recur (read rdr))))
     (catch RuntimeException _)))
 
-(defn sexpr-type
-  [sexpr]
-  (let [sexpr (macroexpand sexpr)]
-    ;; Only when things are interesting
-    (when (>= (count sexpr) 3)
-      (let [fst (first sexpr)
-            sec (second sexpr)
-            thd (third sexpr)]
-        (cond
-         ;; We found a def or defn
-         (= 'def fst)
-         [:def sexpr]
-         ;; We found a defmacro
-         (and (= 'do fst) (= 'clojure.core/defn (first sec))
-              (= 'setMacro (first (third thd))))
-         [:defmacro sexpr]
-         ;; We found a defmethod
-         (and (= '. fst) (= 'clojure.core/addMethod thd))
-         [:defmethod sexpr])))))
-
 (defn rand-str
-  [])
+  []
+  (apply str
+         (repeatedly (+ 2 (rand-int 10))
+                     #(char (+ 33 (rand-int 25))))))
 
 (defmulti mangle
   "Change a 'primitive'"
@@ -77,13 +60,18 @@
 
 (defmethod mangle
   Long
-  [n]
+  [_]
   (- (rand-int 2001)
      1001))
 
 (defmethod mangle
+  Character
+  [_]
+  (char (+ 33 (rand-int 94))))
+
+(defmethod mangle
   Double
-  [n]
+  [_]
   (double (- (rand-int 2001)
              1001)))
 
@@ -112,17 +100,63 @@
 (defmethod mangle
   :map
   [m]
-  (into {} (map (fn [k v] [(mangle k) (mangle v)])
+  (into {} (map (fn [[k v]] [(mangle k) (mangle v)])
                 m)))
 
-(defn distort
-  [[type sexpr]]
-  (case type
-   :def
-   :defmacro
-   :defmethod))
+(defmethod mangle
+  :default
+  [x]
+  (prn "I failed:" x)
+  x)
 
-(declare distort)
+(defn update-in-list
+  [x idxs f & args]
+  (if-let [i (first idxs)]
+    (loop [n 0 acc () rem x]
+      (cond (== i n)
+            (recur (inc n)
+                   (conj acc (apply update-in-list
+                                    (first rem) (rest idxs) f args))
+                   (rest rem))
+            (empty? rem)
+            (reverse acc)
+            :else
+            (recur (inc n)
+                   (conj acc (first rem))
+                   (rest rem))))
+    (apply f x args)))
+
+(defn sexpr-type
+  [sexpr]
+  (let [sexpr (macroexpand sexpr)]
+    ;; Only when things are interesting
+    (when (>= (count sexpr) 3)
+      (let [fst (first sexpr)
+            sec (second sexpr)
+            thd (third sexpr)]
+        (cond
+         ;; We found a def or defn
+         (= 'def fst)
+         [:def sexpr]
+         ;; We found a defmacro
+         (and (= 'do fst) (= 'clojure.core/defn (first sec))
+              (= 'setMacro (first (third thd))))
+         [:defmacro sexpr]
+         ;; We found a defmethod
+         (and (= '. fst) (= 'clojure.core/addMethod thd))
+         [:defmethod sexpr])))))
+
+(defn distort
+  [sexpr]
+  ;; Ignore stuff we can't modify
+  (when (seq? sexpr)
+    (let [[type sexpr1] (sexpr-type sexpr)]
+      (case type
+        :def       (update-in-list sexpr1 [2] mangle)
+        :defmacro  (update-in-list sexpr1 [1] mangle)
+        :defmethod (update-in-list sexpr1 [4] mangle)
+        nil))))
+
 (defn chaos
   [f]
   (with-open [in (PushbackReader. (io/reader f))]
@@ -135,11 +169,13 @@
         (try
           (loop [next-form (read in)]
             (swap! distorted
-                   (fn [form]
-                     (if-let [d (distort next-form)]
-                       (conj form d)
-                       form)))
+                   (fn [acc nxt]
+                     (if-let [d (distort nxt)]
+                       (conj acc d)
+                       acc))
+                   next-form)
             (recur (read in)))
+          ;; EOF :-)
           (catch RuntimeException _
             (reverse @distorted)))))))
 
